d84758dc8a27aed842e2f4a92349ebc0e6f3fe05
PatilNinad18 <iampatil1804@gmail.com>
2025-07-16 16:26:41 +0530

    Did 2 basic projects

---
+++ 04bgColor/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useState } from 'react';
+
+function App() {
+  const [color, setColor] = useState("red");
+
+  return (
+    <div
+      className='w-full h-screen duration-200'
+      style={{ backgroundColor: color }}
+    ></div>
+  );
+}
+
+export default App;
